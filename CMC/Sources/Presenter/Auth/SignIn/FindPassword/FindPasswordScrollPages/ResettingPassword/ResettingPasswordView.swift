98a1abba5d589b6a80d5ab4093b8580bcac5ab22
ir.__.si <yongin9805@gmail.com>
2023-11-20 16:13:51 +0900

    ✨ Feat: #40 - 비밀번호 찾기/인증코드 보내기 완료 Presenter 생성

---
+++ CMC/Sources/Presenter/Auth/SignIn/FindPassword/FindPasswordScrollPages/ResettingPassword/ResettingPasswordView.swift
@@ -0,0 +1,9 @@
+//
+//  ResettingPasswordView.swift
+//  CMC
+//
+//  Created by Siri on 11/15/23.
+//  Copyright © 2023 com.softsquared.cmc. All rights reserved.
+//
+
+import Foundation
---
+++ CMC/Sources/Presenter/Auth/SignIn/FindPassword/FindPasswordScrollPages/ResettingPassword/ResettingPasswordViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  ResettingPasswordViewModel.swift
+//  CMC
+//
+//  Created by Siri on 11/15/23.
+//  Copyright © 2023 com.softsquared.cmc. All rights reserved.
+//
+
+import Foundation
