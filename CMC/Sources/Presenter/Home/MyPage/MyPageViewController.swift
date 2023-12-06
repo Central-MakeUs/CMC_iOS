e77e27ab554b22aa64810dde8cbb9bc6535435fa
ir.__.si <yongin9805@gmail.com>
2023-12-06 11:41:33 +0900

    feat: 내정보 UI 제작

---
+++ CMC/Sources/Presenter/Home/MyPage/MyPageViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MyPageViewController.swift
+//  CMC
+//
+//  Created by Siri on 12/6/23.
+//  Copyright © 2023 com.softsquared.cmc. All rights reserved.
+//
+
+import Foundation
---
+++ CMC/Sources/Presenter/Home/MyPage/MyPageViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  MyPageViewModel.swift
+//  CMC
+//
+//  Created by Siri on 12/6/23.
+//  Copyright © 2023 com.softsquared.cmc. All rights reserved.
+//
+
+import Foundation
