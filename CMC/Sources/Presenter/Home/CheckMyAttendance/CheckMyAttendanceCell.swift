e43847fa050dc65225d56327937f82a964084105
ir.__.si <yongin9805@gmail.com>
2023-12-10 20:18:14 +0900

    feat: 전체 출석 확인 detail cell UI제작

--- CMC/Sources/Presenter/Home/CheckMyAttendance/CheckMyAttendanceCell.swift
+++ CMC/Sources/Presenter/Home/CheckMyAttendance/CheckMyAttendanceCell.swift
@@ -6,194 +6,322 @@
 //  Copyright © 2023 com.softsquared.cmc. All rights reserved.
 //
 
-import UIKit
+import Foundation
+
 import RxSwift
 import RxCocoa
 import RxGesture
 
+import SnapKit
+
+import UIKit
+import DesignSystem
+
 class CheckMyAttendanceCell: UITableViewCell {
-		static let reuseIdentifier = "CheckMyAttendanceCell"
+	static let reuseIdentifier = "CheckMyAttendanceCell"
+	
+	// MARK: - UI
+	private lazy var realContentView: UIView = {
+		let view = UIView()
+		view.backgroundColor = .clear
+		return view
+	}()
+	
+	private lazy var leftCoverView: UIView = {
+		let view = UIView()
+		view.backgroundColor = .clear
+		return view
+	}()
+	
+	private lazy var rightCoverView: UIView = {
+		let view = UIView()
+		view.backgroundColor = .clear
+		return view
+	}()
+	
+	private lazy var weekLabel: UILabel = {
+		let label = UILabel()
+		label.font = CMCFontFamily.Pretendard.bold.font(size: 20)
+		if model.enable {
+			label.textColor = CMCAsset.gray50.color
+		} else {
+			label.textColor = CMCAsset.gray800.color
+		}
+		label.text = "\(model.week)" + "주차"
+		return label
+	}()
+	
+	private lazy var dataStackView: UIStackView = {
+		let stackView = UIStackView()
+		stackView.axis = .horizontal
+		stackView.spacing = 10
+		stackView.alignment = .center
+		stackView.addArrangedSubview(dateImage)
+		stackView.addArrangedSubview(dateLabel)
+		return stackView
+	}()
+	
+	private lazy var dateImage: UIImageView = {
+		let imageView = UIImageView()
+		if model.enable {
+			imageView.image = CMCAsset._14x14attendanceDateValid.image
+		} else {
+			imageView.image = CMCAsset._14x14attendanceDateInvalid.image
+		}
+		return imageView
+	}()
+	
+	private lazy var dateLabel: UILabel = {
+		let label = UILabel()
+		label.font = CMCFontFamily.Pretendard.medium.font(size: 13)
+		if model.enable {
+			label.textColor = CMCAsset.gray700.color
+		} else {
+			label.textColor = CMCAsset.gray800.color
+		}
+		label.text = onlyDate(date: "\(model.date)")
+		return label
+	}()
+	
+	private lazy var isOfflineLabel: CMCButton = {
+		let title = model.isOffline ? "Offline" : "Online"
+		let type = model.enable
+		? CMCButton.CMCButtonType.login(.none)
+		: CMCButton.CMCButtonType.login(.disabled)
+		let button = CMCButton(
+			isRound: false,
+			iconTitle: nil,
+			type: type,
+			title: title
+		)
+		return button
+	}()
+	
+	private lazy var firstHourStackView: UIStackView = {
+		let stackView = UIStackView()
+		stackView.axis = .vertical
+		stackView.spacing = 20
+		stackView.alignment = .center
+		stackView.addArrangedSubview(firstAttendanceImageView)
+		stackView.addArrangedSubview(firstAttendanceLabel)
+		return stackView
+	}()
+	
+	private lazy var firstAttendanceImageView: UIImageView = {
+		let imageView = UIImageView()
+		switch model.firstHour {
+		case "ABSENT":
+			imageView.image = CMCAsset._20x20attendanceAbsent.image
+		case "LATE":
+			imageView.image = CMCAsset._20x20attendanceLate.image
+		default:
+			imageView.image = CMCAsset._20x20attendanceValid.image
+		}
+		if !model.enable {
+			imageView.image = CMCAsset._20x20attendanceAbsent.image
+		}
+		imageView.contentMode = .scaleAspectFit
+		return imageView
+	}()
+	
+	private lazy var firstAttendanceLabel: UILabel = {
+		let label = UILabel()
+		label.text = "1차"
+		label.font = CMCFontFamily.Pretendard.bold.font(size: 13)
+		switch model.firstHour {
+		case "ABSENT":
+			label.textColor = CMCAsset.gray800.color
+		case "LATE":
+			label.textColor = CMCAsset.error.color
+		default:
+			label.textColor = CMCAsset.gray50.color
+		}
+		if !model.enable {
+			label.textColor = CMCAsset.gray800.color
+		}
+		return label
+	}()
+	
+	private lazy var centerSeparator: UIView = {
+		let view = UIView()
+		view.backgroundColor = CMCAsset.gray800.color
+		return view
+	}()
+	
+	private lazy var secondHourStackView: UIStackView = {
+		let stackView = UIStackView()
+		stackView.axis = .vertical
+		stackView.spacing = 20
+		stackView.alignment = .center
+		stackView.addArrangedSubview(secondAttendanceImageView)
+		stackView.addArrangedSubview(secondAttendanceLabel)
+		return stackView
+	}()
+	
+	
+	private lazy var secondAttendanceImageView: UIImageView = {
+		let imageView = UIImageView()
+		switch model.secondHour {
+		case "ABSENT":
+			imageView.image = CMCAsset._20x20attendanceAbsent.image
+		case "LATE":
+			imageView.image = CMCAsset._20x20attendanceLate.image
+		default:
+			imageView.image = CMCAsset._20x20attendanceValid.image
+		}
+		if !model.enable {
+			imageView.image = CMCAsset._20x20attendanceAbsent.image
+		}
+		imageView.contentMode = .scaleAspectFit
+		return imageView
+	}()
+	
+	private lazy var secondAttendanceLabel: UILabel = {
+		let label = UILabel()
+		label.text = "2차"
+		label.font = CMCFontFamily.Pretendard.bold.font(size: 13)
+		switch model.secondHour {
+		case "ABSENT":
+			label.textColor = CMCAsset.gray800.color
+		case "LATE":
+			label.textColor = CMCAsset.error.color
+		default:
+			label.textColor = CMCAsset.gray50.color
+		}
+		if !model.enable {
+			label.textColor = CMCAsset.gray800.color
+		}
+		return label
+	}()
+	
+	private lazy var bottomSeparator: UIView = {
+		let view = UIView()
+		view.backgroundColor = CMCAsset.gray800.color
+		return view
+	}()
+	
+	// MARK: - Properties
+	private let model: AttendanceDetailsModel
+	private var disposeBag = DisposeBag()
+	
+	// MARK: - Initialize
+	
+	init(
+		style: UITableViewCell.CellStyle,
+		reuseIdentifier: String?,
+		model: AttendanceDetailsModel
+	) {
+		self.model = model
+		super.init(style: .default, reuseIdentifier: reuseIdentifier)
+		setAddSubviews()
+		setAddConstraints()
+	}
+	
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	//MARK: - Methods
+	private func setAddSubviews() {
+		contentView.addSubview(realContentView)
 		
-		// MARK: - UI
-		private lazy var allView: UIView = {
-				let view = UIView()
-				view.translatesAutoresizingMaskIntoConstraints = false
-				view.backgroundColor = .clear
-				return view
-		}()
+		realContentView.addSubview(leftCoverView)
+		realContentView.addSubview(rightCoverView)
 		
-		private lazy var mainImageView: UIImageView = {
-				let imageView = UIImageView()
-				imageView.translatesAutoresizingMaskIntoConstraints = false
-				imageView.backgroundColor = .clear
-				imageView.contentMode = .scaleAspectFit
-				return imageView
-		}()
+		leftCoverView.addSubview(weekLabel)
+		leftCoverView.addSubview(dataStackView)
+		leftCoverView.addSubview(isOfflineLabel)
 		
-		private lazy var eventStyleLabel: UILabel = {
-				let label = UILabel()
-				label.translatesAutoresizingMaskIntoConstraints = false
-				label.backgroundColor = .clear
-				label.textColor = .white
-				label.font = UIFont.systemFont(ofSize: 13, weight: .medium)
-				return label
-		}()
+		rightCoverView.addSubview(firstHourStackView)
+		rightCoverView.addSubview(centerSeparator)
+		rightCoverView.addSubview(secondHourStackView)
 		
-		private lazy var eventInfoLabel: UILabel = {
-				let label = UILabel()
-				label.translatesAutoresizingMaskIntoConstraints = false
-				label.backgroundColor = .clear
-				label.textColor = .white
-				label.numberOfLines = 2
-				label.font = UIFont.systemFont(ofSize: 17, weight: .semibold)
-				return label
-		}()
+		contentView.addSubview(bottomSeparator)
+	}
+	
+	private func setAddConstraints() {
 		
-		private lazy var eventDataLabel: UILabel = {
-				let label = UILabel()
-				label.translatesAutoresizingMaskIntoConstraints = false
-				label.backgroundColor = .clear
-				label.textColor = .systemColorGrayGray300
-				label.font = UIFont.systemFont(ofSize: 12, weight: .regular)
-				return label
-		}()
-		
-		private lazy var seperatorView: UIView = {
-				let view = UIView()
-				view.translatesAutoresizingMaskIntoConstraints = false
-				view.backgroundColor = .systemColorGrayGray600
-				return view
-		}()
-		
-		private lazy var eventTimeLabel: UILabel = {
-				let label = UILabel()
-				label.translatesAutoresizingMaskIntoConstraints = false
-				label.backgroundColor = .clear
-				label.textColor = .systemColorGrayGray300
-				label.font = UIFont.systemFont(ofSize: 12, weight: .regular)
-				return label
-		}()
-		
-		private lazy var devider: UIView = {
-				let view = UIView()
-				view.translatesAutoresizingMaskIntoConstraints = false
-				view.backgroundColor = .separatorPrimaryDark
-				return view
-		}()
-		
-		private var disposeBag = DisposeBag()
-		
-		// MARK: - Initialize
-		
-		override init(
-				style: UITableViewCell.CellStyle,
-				reuseIdentifier: String?
-		) {
-				super.init(style: style, reuseIdentifier: reuseIdentifier)
-				setAddSubviews()
-				setAddConstraints()
-				self.backgroundColor = .black
+		realContentView.snp.makeConstraints { make in
+			make.leading.equalToSuperview().offset(25)
+			make.trailing.equalToSuperview().offset(19)
+			make.top.equalToSuperview().offset(25)
+			make.bottom.equalToSuperview().offset(-25)
 		}
 		
-		required init?(coder: NSCoder) {
-				fatalError("init(coder:) has not been implemented")
+		leftCoverView.snp.makeConstraints { make in
+			make.top.leading.bottom.equalToSuperview()
+			make.width.equalToSuperview().multipliedBy(0.34)
 		}
 		
-		//MARK: - Methods
-		private func setAddSubviews() {
-				self.contentView.addSubview(allView)
-				self.allView.addSubview(mainImageView)
-				self.allView.addSubview(eventStyleLabel)
-				self.allView.addSubview(eventInfoLabel)
-				self.allView.addSubview(eventDataLabel)
-				self.allView.addSubview(seperatorView)
-				self.allView.addSubview(eventTimeLabel)
-				self.allView.addSubview(devider)
+		weekLabel.snp.makeConstraints { make in
+			make.top.equalToSuperview().offset(25)
+			make.centerX.equalToSuperview()
 		}
 		
-		private func setAddConstraints() {
-				NSLayoutConstraint.activate([
-						
-						allView.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: 16),
-						allView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor, constant: -16),
-						allView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: 16),
-						allView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: -16),
-						
-						mainImageView.topAnchor.constraint(equalTo: self.allView.topAnchor),
-						mainImageView.leadingAnchor.constraint(equalTo: self.allView.leadingAnchor),
-						mainImageView.widthAnchor.constraint(equalToConstant: 84),
-						mainImageView.heightAnchor.constraint(equalToConstant: 84),
-						
-						eventStyleLabel.topAnchor.constraint(equalTo: mainImageView.topAnchor),
-						eventStyleLabel.leadingAnchor.constraint(equalTo: mainImageView.trailingAnchor, constant: 16),
-						
-						eventInfoLabel.topAnchor.constraint(equalTo: eventStyleLabel.bottomAnchor, constant: 4),
-						eventInfoLabel.leadingAnchor.constraint(equalTo: eventStyleLabel.leadingAnchor),
-						eventInfoLabel.trailingAnchor.constraint(equalTo: self.allView.trailingAnchor),
-						
-						eventDataLabel.bottomAnchor.constraint(equalTo: self.allView.bottomAnchor),
-						eventDataLabel.leadingAnchor.constraint(equalTo: eventStyleLabel.leadingAnchor),
-						
-						seperatorView.leadingAnchor.constraint(equalTo: eventDataLabel.trailingAnchor, constant: 6),
-						seperatorView.centerYAnchor.constraint(equalTo: eventDataLabel.centerYAnchor),
-						seperatorView.heightAnchor.constraint(equalToConstant: 8),
-						seperatorView.widthAnchor.constraint(equalToConstant: 1),
-						
-						eventTimeLabel.leadingAnchor.constraint(equalTo: seperatorView.trailingAnchor, constant: 6),
-						eventTimeLabel.bottomAnchor.constraint(equalTo: self.allView.bottomAnchor),
-						
-						devider.leadingAnchor.constraint(equalTo: self.allView.leadingAnchor),
-						devider.trailingAnchor.constraint(equalTo: self.allView.trailingAnchor),
-						devider.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor),
-						devider.heightAnchor.constraint(equalToConstant: 1)
-				
-				])
+		dateImage.snp.makeConstraints { make in
+			make.height.width.equalTo(14)
 		}
 		
-		func configureCell(eventList: CSEventListModel) {
-				if eventList.isCaption {
-						eventStyleLabel.text = "real_time_captioning".localized()
-						eventStyleLabel.textColor = UIColor(hex: 0x55A5F6)
-						eventInfoLabel.text = eventList.title
-						eventDataLabel.text = onlyDate(data: eventList.dateAtUtc)
-						eventTimeLabel.text = onlyTime(data: eventList.dateAtUtc)
-						mainImageView.image = UIImage(named: "_24x24Caption")
-				} else {
-						eventStyleLabel.text = "_scoreboard_info_default".localized()
-						eventStyleLabel.textColor = UIColor(hex: 0x93DC00)
-						eventInfoLabel.text = eventList.title
-						eventDataLabel.text = onlyDate(data: eventList.dateAtUtc)
-						eventTimeLabel.text = onlyTime(data: eventList.dateAtUtc)
-						if let data = eventList.thumbNail {
-								mainImageView.image = UIImage(data: data)
-						}
-				}
+		dataStackView.snp.makeConstraints { make in
+			make.centerX.equalTo(weekLabel)
+			make.top.equalTo(weekLabel).offset(12)
 		}
 		
-		private func bind() {
-				
+		isOfflineLabel.snp.makeConstraints { make in
+			make.leading.trailing.bottom.equalToSuperview()
+			make.height.equalToSuperview().multipliedBy(0.26)
 		}
 		
+		rightCoverView.snp.makeConstraints { make in
+			make.top.bottom.trailing.equalToSuperview()
+			make.width.equalToSuperview().multipliedBy(0.42)
+		}
+		
+		firstAttendanceImageView.snp.makeConstraints { make in
+			make.height.width.equalTo(20)
+		}
+		
+		firstHourStackView.snp.makeConstraints { make in
+			make.leading.equalToSuperview()
+			make.centerY.equalToSuperview()
+		}
+		
+		centerSeparator.snp.makeConstraints { make in
+			make.centerY.equalTo(firstAttendanceImageView)
+			make.centerX.equalToSuperview()
+			make.height.equalTo(1)
+		}
+		
+		secondAttendanceImageView.snp.makeConstraints { make in
+			make.height.width.equalTo(20)
+		}
+		
+		secondHourStackView.snp.makeConstraints { make in
+			make.trailing.equalToSuperview()
+			make.centerY.equalToSuperview()
+		}
+		
+		bottomSeparator.snp.makeConstraints { make in
+			make.leading.trailing.bottom.equalToSuperview()
+			make.height.equalTo(1)
+		}
+	}
 }
 
-extension CSEventTableViewCell {
-		
-		func onlyDate(data: String) -> String {
-				let dateFormatter = DateFormatter()
-				dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
-				let date = dateFormatter.date(from: data)
-				dateFormatter.dateFormat = "dd MMM. yyyy"
-				dateFormatter.locale = Locale(identifier: "en_US_POSIX")
-				let dateString = dateFormatter.string(from: date ?? Date())
-				return dateString
-		}
-		
-		func onlyTime(data: String) -> String {
-				let dateFormatter = DateFormatter()
-				dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
-				let date = dateFormatter.date(from: data)
-				dateFormatter.dateFormat = "HH:mm"
-				let dateString = dateFormatter.string(from: date ?? Date())
-				return dateString
+
+extension CheckMyAttendanceCell{
+		func onlyDate(date: String) -> String {
+			let inputFormatter = DateFormatter()
+			inputFormatter.dateFormat = "yyyy-MM-dd"
+
+			if let date = inputFormatter.date(from: date) {
+					let outputFormatter = DateFormatter()
+					outputFormatter.dateFormat = "MM.dd"
+					
+					let formattedDateString = outputFormatter.string(from: date)
+					return formattedDateString
+			} else {
+					return date
+			}
 		}
 }
